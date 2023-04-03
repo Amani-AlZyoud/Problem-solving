a00ec57a747fe55952cff7935fdec5e9a992e980
Amani-AlZyoud <Amani.shzyoud@gmail.com>
2023-04-03 18:37:13 +0300

    Data Structure/LinkedList 03/04/2023, 6:36 PM

--- app.js
+++ app.js
@@ -52,7 +52,8 @@ console.log(`Reversed String: ${reverseString("Amani", 5)}`); // Amani -> inamA
 
 
 
-// DATA STRUCTURE STACK AND QUEUE
+
+// ********************************DATA STRUCTURE STACK AND QUEUE**************************************
 // 1. Implement a function that returns the minimum element in a stack in constant time complexity O(1).
 
 function getMin() {
@@ -79,25 +80,18 @@ push(56);
 push(10);
 push(95);
 push(100);
-console.log(`Stack: ${s}`);
+console.log(`\nStack: ${s}`);
 getMin();
-
-
-
 
 
 // 2. Given a queue of integers, reverse the order of the elements in the queue.
 
 function reverse(queue) {
- 
-
   // output queue
   let reverseQueue = [];
 
-  for (let i = (queue.length -1); i >= 0; i--) {
-
-     reverseQueue.push(queue[i]);
-
+  for (let i = queue.length - 1; i >= 0; i--) {
+    reverseQueue.push(queue[i]);
   }
   return reverseQueue;
 }
@@ -111,11 +105,17 @@ queue.push(3);
 queue.push(4);
 queue.push(5);
 
-console.log(`Queue: ${queue}`);
+console.log(`\nQueue: ${queue}`);
 queue = reverse(queue);
 
 // Print the Queue
 console.log(`Reversed Queue: ${queue}`);
+
+
+
+
+
+
 
 
 
@@ -124,41 +124,182 @@ console.log(`Reversed Queue: ${queue}`);
 let s1 = [];
 let s2 = [];
 
-
-
 function enqueue(element) {
   s1.push(element);
-
 }
-
 
 function dequeue() {
   if (s2.length === 0) {
-    if (s1.length === 0) { return 'Cannot dequeue because queue is empty'; }
+    if (s1.length === 0) {
+      return "Cannot dequeue because queue is empty";
+    }
     while (s1.length > 0) {
       let p = s1.pop();
       s2.push(p);
     }
   }
 
-
   let deqEle = s2.pop();
   while (s2.length > 0) {
     let p = s2.pop();
     s1.push(p);
-  
-}
-return deqEle;
-
+  }
+  return deqEle;
 }
 
-enqueue('a');
-enqueue('b');
-enqueue('c');
-enqueue('d');
-enqueue('e');
+enqueue("a");
+enqueue("b");
+enqueue("c");
+enqueue("d");
+enqueue("e");
 
 console.log(`Queue using 2 stacks s1 and s2: ${s1}`);
-console.log(`Dequeuing Element: ${dequeue()}`); 
+console.log(`Dequeuing Element: ${dequeue()}`);
 // dequeue();
 console.log(`Queue using 2 stacks s1 and s2: ${s1}`);
+
+
+
+
+
+// ***************************************LINKED LIST-DATA STRUCTURE***************************************
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  insert(newNode) {
+    let node = this.head;
+    if (node == null) {
+      this.head = newNode;
+
+      return;
+    }
+    while (node.next) {
+      node = node.next;
+    }
+    node.next = newNode;
+  }
+
+  printList() {
+    let node = this.head;
+    const arr = [];
+    while (node) {
+      arr.push(node.value);
+      node = node.next;
+    }
+
+    console.log(...arr);
+    return arr;
+  }
+
+  listSize() {
+    let size = 0;
+    let node = this.head;
+    while (node) {
+      size++;
+      node = node.next;
+    }
+    return size;
+  }
+}
+
+
+
+
+// 4. Create a function that takes a LinkedList and deletes the middle node in it and returns it.
+
+function deleteMiddleNode(list) {
+  
+  let firstIndex = 0;
+  let lastIndex = list.listSize()-1;
+  let index = Math.ceil(firstIndex + (lastIndex - firstIndex) / 2);
+  let del;
+ 
+  let node = list.head;
+    if(index==0) {
+        if(node!==null)
+        {
+            del = node.value;
+            node = node.next;
+            
+            list.head = node;
+            
+        }
+     
+        return del;
+    }
+     let i = 0;
+
+    while (node.next!==null && i < index-1)
+    {
+      node = node.next;
+      i++;
+    }
+ 
+    del = node.next;
+
+    node.next = node.next.next;
+    console.log(`\nDeleted node: At Middle index = ${index}`);
+    return del;
+
+
+}
+
+
+
+// 5. Create a function that takes a LinkedList and reverses it.
+
+function reverseList(list) {
+  let node = list.head;
+  let prev = null;
+  let next = null;
+  while (node) { // 1 2 3 4 
+    next = node.next; // 2 3 4 null
+    node.next = prev; // 4 -> 3 -> 2 -> 1 -> null
+    prev = node;  // 1 2 3 4
+    node = next; //  2 3 4 null
+  }
+  list.head = prev;
+  console.log("\nReversed LinkedList:");
+  return list;
+
+}
+
+
+
+
+const list = new LinkedList();
+
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
+
+
+list.insert(node1);
+list.insert(node2);
+list.insert(node3);
+list.insert(node4);
+list.insert(node5);
+
+
+console.log("\nLinkedList:");
+list.printList();
+let deleted = deleteMiddleNode(list);
+console.log(deleted);
+console.log("\nAfter deletion:");
+list.printList();
+
+reverseList(list);
+
+list.printList();
